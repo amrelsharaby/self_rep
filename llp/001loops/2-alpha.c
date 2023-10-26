80b23476d88b304e4ea1dfd5fff6d442998b4e8e
zawayaalex <141460715+zawayaalex@users.noreply.github.com>
2023-10-26 05:37:33 +0300

    loops in simple way

---
+++ llp/001loops/2-alpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Description: putchar to print
+* Return: 0
+*/
+int main(void)
+{
+char c;
+    for (c = 'a' ; c <= 'z' ; c++)
+    {
+putchar(c);
+    }
+    putchar('\n');
+}
---
+++ llp/001loops/quiz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+int b = 3;
+int a =0;
+for (a = 0; a < b; a++)
+{
+    printf("%d\n", a);
+}
+}
