17f89ed0a3dd1b028226c450022339a1afb688a8
zawayaalex <141460715+zawayaalex@users.noreply.github.com>
2023-10-28 11:55:56 +0200

    nested loops tested

---
+++ llp/002-nested-loops/2-alpha.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+void print_alphabet_x10(void)
+
+for (int l = 0; l < 10; l++)
+{
+    for (int n = 97; n < 123; n++)
+    {
+        _putchar(n);
+    }
+    _putchar('\n');
+
+}
---
+++ llp/002-nested-loops/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+for (int l = 0; l < 10; l++)
+{
+    for (int n = 97; n < 123; n++)
+    {
+        putchar(n);
+    }
+    putchar('\n');
+
+}
+}
