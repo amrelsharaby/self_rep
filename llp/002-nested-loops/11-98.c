909ee2b4aa53388b8805da7a1ac8e948ea367a71
zawayaalex <141460715+zawayaalex@users.noreply.github.com>
2023-10-28 14:49:49 +0200

    nested loops 2.0

--- llp/002-nested-loops/11-98.c
+++ llp/002-nested-loops/11-98.c
@@ -2,5 +2,20 @@ void print_to_98(int n);
 
 if (n > 98)
 {
-    for (n; n)
+    for (int m = n; m > 98; m--)
+    {
+        printf("%i, ",m);
+    }
+    printf("98\n");
+}
+else if (n < 98)
+{
+    for (int m = n; m < 98; m++)
+    {
+        printf("%i, ",m);
+    }
+    printf("98\n");
+}
+else
+printf("98\n");
 }
--- llp/002-nested-loops/test.c
+++ llp/002-nested-loops/test.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int print_last_digit(int n);
 
 int main(void)
 {
-    int n = 0;
-    int result = print_last_digit(n);
+    int n = 98;
 
-printf("%i\n",result);
-    /** printf("Is %c a lowercase character? %s\n", c, result ? "Yes" : "No");
-*/
-    return (0);
-}
-
-int print_last_digit(int n)
+if (n > 98)
 {
-    if (n >= 0)
-        return (n % 10);
-    return (-n % 10);
+    for (int m = n; m > 98; m--)
+    {
+        printf("%i, ",m);
+    }
+    printf("98\n");
+}
+else if (n < 98)
+{
+    for (int m = n; m < 98; m++)
+    {
+        printf("%i, ",m);
+    }
+    printf("98\n");
+}
+else
+printf("98\n");
 }
